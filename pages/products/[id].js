b2c76529bcfeec7a514cf0cd1d72e00449e673af
lilytsai503-prog <lilytsai503@gmail.com>
2025-12-28 22:09:21 +0800

    Create dynamic product page for Sofa component

---
+++ pages/products/[id].js
@@ -0,0 +1,13 @@
+import { useRouter } from 'next/router';
+
+export default function Sofa() {
+  const { id } = useRouter().query;
+
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>沙發編號 {id}</h1>
+      <p>可客製尺寸與顏色</p>
+      <button>加入購物車</button>
+    </div>
+  );
+}
